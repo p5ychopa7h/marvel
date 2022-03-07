c754fa349137df9c9caece433242f60dc8340f0d
p5ychopa7h <eshayzanov@mail.ru>
2022-03-08 04:13:28 +0600

    5 commit]

--- marvel/src/components/charList/CharList.js
+++ marvel/src/components/charList/CharList.js
@@ -1,46 +1,17 @@
 import './charList.scss';
 import abyss from '../../resources/img/abyss.jpg';
+import CharListItem from '../charListItem/CharListItem';
 
 const CharList = () => {
     return (
         <div className="char__list">
             <ul className="char__grid">
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item char__item_selected">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
+            <CharListItem />
+            <CharListItem />
+            <CharListItem />
+            <CharListItem />
+            <CharListItem />
+            <CharListItem />
             </ul>
             <button className="button button__main button__long">
                 <div className="inner">load more</div>
