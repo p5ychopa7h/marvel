cc6bc014201c94ebb540dcedc70d4f2a89c4948d
p5ychopa7h <eshayzanov@mail.ru>
2022-03-11 22:20:56 +0600

    10 commit

--- marvel/src/components/charInfo/CharInfo.js
+++ marvel/src/components/charInfo/CharInfo.js
@@ -27,6 +27,7 @@ class CharInfo extends Component {
         }
     }
 
+
     updateChar = () => {
         const {charId} = this.props;
         if (!charId) {
@@ -39,6 +40,7 @@ class CharInfo extends Component {
             .getCharacter(charId)
             .then(this.onCharLoaded)
             .catch(this.onError);
+
     }
 
     onCharLoaded = (char) => {
